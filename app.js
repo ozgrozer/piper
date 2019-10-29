11bab3970035ce65c9f0c32a54fb3d103345384a
Ozgur Ozer <ozgr@live.com>
2019-10-29 22:38:36 +0300

    Capture range values.

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log('test')
+const listener = function () {
+  window.requestAnimationFrame(() => {
+    const getValue = this.value
+    if (getValue) {
+      const getId = this.getAttribute('data-id')
+      const optionValue = document.getElementById(getId + 'Value')
+      optionValue.innerHTML = getValue
+    }
+  })
+}
+
+const ranges = document.getElementsByClassName('optionRange')
+for (let i = 0; i < ranges.length; i++) {
+  const range = ranges[i]
+  range.addEventListener('mousedown', () => {
+    listener()
+    range.addEventListener('mousemove', listener)
+  })
+  range.addEventListener('mouseup', () => {
+    range.removeEventListener('mousemove', listener)
+  })
+  range.addEventListener('keydown', listener)
+  range.addEventListener('change', listener)
+}
